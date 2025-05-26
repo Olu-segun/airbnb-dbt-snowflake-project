912e145d0a176c0a9ff0e4e0cc163c8a874a2bcf
olu-segun <olukayodeoluseguno@gmail.com>
2025-05-26 15:01:46 +0100

    Added dbt tests and increamental model

--- airbnb/macros/no_nulls_in_columns.sql
+++ airbnb/macros/no_nulls_in_columns.sql
@@ -1,3 +1,8 @@
-{% macro non_nulls_in_columns() %}
-  
+{% macro no_nulls_in_columns(model) %}
+    SELECT * FROM {{ model }} WHERE
+    {% for col in adapter.get_columns_in_relation(model) -%}
+        {{ col.column }} IS NULL OR
+    {% endfor %}
+    FALSE
 {% endmacro %}
+
--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -1,8 +1,8 @@
-{{
+{{ 
   config(
     materialized = 'incremental',
     on_schema_change = 'fail'
-    )
+  )
 }}
 
 WITH src_reviews AS (
@@ -10,10 +10,11 @@ WITH src_reviews AS (
     FROM {{ ref("src_reviews") }}
 )
 
-SELECT *
+SELECT 
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
+  *
 FROM src_reviews
 WHERE review_text IS NOT NULL
 {% if is_incremental() %}
-  and review_date >=(select max(review_date) from {{ this }})
+  AND review_date >= (SELECT MAX(review_date) FROM {{ this }})
 {% endif %}
-  
